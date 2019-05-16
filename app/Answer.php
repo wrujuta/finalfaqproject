8632096130e923cf7b671410805d1d9b9e6dc1e3
wrujuta <wpd2@njit.edu>
2019-05-15 21:35:38 -0400

    Fixed branding link

---
+++ app/Answer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+
+    protected $fillable = ['body'];
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    public function question()
+    {
+        return $this->belongsTo('App\Question');
+    }
+}
